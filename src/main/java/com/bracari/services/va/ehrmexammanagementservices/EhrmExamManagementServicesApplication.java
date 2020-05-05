f49b3e204afa0e17c89fd38eb0a0c4bce4710cef
Pete Grazaitis <pgrazaitis@gmail.com>
2020-05-05 08:32:43 -0400

    Spring Initialzr baseline

---
+++ src/main/java/com/bracari/services/va/ehrmexammanagementservices/EhrmExamManagementServicesApplication.java
@@ -0,0 +1,13 @@
+package com.bracari.services.va.ehrmexammanagementservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhrmExamManagementServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhrmExamManagementServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bracari/services/va/ehrmexammanagementservices/EhrmExamManagementServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bracari.services.va.ehrmexammanagementservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhrmExamManagementServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
